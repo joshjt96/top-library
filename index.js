bcf12ff159a7e4cf423ec3b4ec8a6648927f051e
Josh Talley <jtdev84@gmail.com>
2022-06-18 18:08:38 +0100

    Manually add objects in array

---
+++ index.js
@@ -0,0 +1,23 @@
+let myLibrary = [
+    {
+        'title': 'Moby Dick',
+        'author': 'Herman Melville',
+        'pages': '427' 
+    },
+    {
+        'title': 'The Hobbit',
+        'author': 'J.R.R Tolkien',
+        'pages': '295'
+    }
+];
+
+function Book(title, author, pages) {
+
+};
+
+function addToLib() {
+
+};
+
+// const mobyDick = new Book('Moby Dick', 'Herman Melville', '427', 'have not')
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'have not')
