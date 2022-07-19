cf91a9c107121db8505211bdb53a94297bcdde39
Josh Talley <jtdev84@gmail.com>
2022-07-19 19:46:08 +0100

    Fix remove button

--- library.js
+++ library.js
@@ -1,7 +1,7 @@
 // Array to store books
 let myLibrary = [
-    { title: 'Moby Dick', author: 'Herman Melville', pages: '427', read: 'No' },
-    { title: 'The Hobbit', author: 'J.R.R Tolkien', pages: '295', read: 'No' },
+    // { title: 'Moby Dick', author: 'Herman Melville', pages: '427', read: 'No' },
+    // { title: 'The Hobbit', author: 'J.R.R Tolkien', pages: '295', read: 'No' },
 ];
 
 // Object constructor
@@ -19,60 +19,50 @@ function addToLib(title, author, pages, read) {
     displayBook();
 };
 
-// Function to display books on page
+// Function to create cards that display the new book objects
 function displayBook() {
-    const books = document.querySelector('.books');
-
-    // for loop to remove previously displayed card before looping over array again
+    const books = document.querySelector('.books')
     const removeCard = document.querySelectorAll('.card');
     for (let i = 0; i < removeCard.length; i++) {
         removeCard[i].remove();
     };
-
-    // ForEach loop to display the object values to the cards
-    myLibrary.forEach(myLibrary => {
+    // Loop over the array and display to the cards
+    let index = 0;
+    myLibrary.forEach(myLibrarys => {
         const card = document.createElement('div');
         card.classList.add('card');
         books.appendChild(card);
-        for (let key in myLibrary) {
-            const para = document.createElement('p');
-            para.textContent = (`${key}: ${myLibrary[key]}`);
-            card.appendChild(para);
-        }
-    });
-    addRemoveBtn()
-};
 
-// Adding button to card to remove book from library and add class attribute for each object card
-function addRemoveBtn() {
-    const cardSelect = document.querySelectorAll('.card');
-    cardSelect.forEach(cardSelect => {
-        const removeBtn = Object.assign(document.createElement('button'), {
-            type: 'button'
-        });
-        removeBtn.classList.add('remove-button');
-        cardSelect.appendChild(removeBtn);
-        removeBtn.textContent = 'Remove Book';
-        // Link the data attribute of the remove button to the array and card
-        let index = Number('0');
-        removeBtn.dataset.library = index;
+        // Create remove book button and add class attribute for each array card
+        const removeBookButton = document.createElement('button');
+        removeBookButton.classList.add('remove-button');
+        removeBookButton.textContent = "Remove Book V2"
+        // console.log('Show me my current array objects inside of foreach....', myLibrary);
+
+        // Link data attribute of the remove button to the array and card
+        removeBookButton.dataset.linkedArray = index;
         index++;
-        console.log(removeBtn.dataset.library);
-        // Event listener to remove object from card, with function to remove object from array
-        removeBtn.addEventListener('click', removeBook());
-        function removeBook() {
-            let bookToRemove = removeBtn.dataset.library;
-            myLibrary.splice(bookToRemove, 1);
-            const cardToRemove = document.querySelector('.card');
-            cardToRemove.remove;
-        }
+        // console.log('Show me the dataset that is linked back to the array...', removeBookButton.dataset.linkedArray);
+        card.appendChild(removeBookButton);
+
+        // Start event listener to remove array item from array and card from parent div via the dataset link
+        removeBookButton.addEventListener('click', removeBookFromLib);
+
+        function removeBookFromLib() {
+            let getBookToRemove = removeBookButton.dataset.linkedArray;
+            myLibrary.splice(parseInt(getBookToRemove), 1);
+            card.remove();
+            displayBook();
+        };
+
+        // Loop over array keys and values and display to each card
+        for (let key in myLibrarys) {
+            const para = document.createElement('p');
+            para.textContent = (`${key}: ${myLibrarys[key]}`);
+            card.appendChild(para);
+        };
     });
 };
-
-// addToLib('The Great Gatsby', 'F Scott Fitzgerald', '427', false);
-// addToLib('Example', 'Noone', '427', false);
-// addToLib('Hello', 'There', '427', false);
-// addToLib('Issme', 'Mario', '427', false);
 
 // New book button that displays form when clicked:
 form.style.display = 'none';
@@ -110,19 +100,58 @@ resetButton.addEventListener('click', () => {
     document.getElementById('book-form').reset();
 });
 
+// My original code: had to change as the 'Remove Book' button was being added multiple times on a card. Think this was caused by the function being 
+// inside an earlier loop 
 
+// Function to display books on page
+// function displayBook() {
+//     const books = document.querySelector('.books');
 
+//     // for loop to remove previously displayed card before looping over array again
+//     // const removeCard = document.querySelectorAll('.card');
+//     // for (let i = 0; i < removeCard.length; i++) {
+//     //     removeCard[i].remove();
+//     // };
 
+//     // ForEach loop to display the object values to the cards
+//     myLibrary.forEach(myLibrary => {
+//         const card = document.createElement('div');
+//         card.classList.add('card');
+//         books.appendChild(card);
+//         for (let key in myLibrary) {
+//             const para = document.createElement('p');
+//             para.textContent = (`${key}: ${myLibrary[key]}`);
+//             card.appendChild(para);
+//         }
+//     });
+//     addRemoveBtn()
+// };
 
-
-
-
-
-
-
-
-
-
+// // Adding button to card to remove book from library and add class attribute for each object card
+// function addRemoveBtn() {
+//     const cardSelect = document.querySelectorAll('.card');
+//     cardSelect.forEach(cardSelect => {
+//         const removeBtn = Object.assign(document.createElement('button'), {
+//             type: 'button'
+//         });
+//         removeBtn.classList.add('remove-button');
+//         cardSelect.appendChild(removeBtn);
+//         removeBtn.textContent = 'Remove Book';
+//         // Link the data attribute of the remove button to the array and card
+//         let index = Number('0');
+//         removeBtn.dataset.library = index;
+//         index++;
+//         console.log(removeBtn.dataset.library);
+//         // Event listener to remove object from card, with function to remove object from array
+//         removeBtn.addEventListener('click', removeBook());
+//         function removeBook() {
+//             let bookToRemove = removeBtn.dataset.library;
+//             myLibrary.splice(bookToRemove, 1);
+//             const cardToRemove = document.querySelector('.card');
+//             cardToRemove.remove;
+//         }
+//     });
+// };
 
 // // Array to store books
 // let myLibrary = [
@@ -180,3 +209,8 @@ resetButton.addEventListener('click', () => {
 // //     bookInfo.textContent = books;
 // //     document.body.appendChild(bookInfo);
 // // })
+
+// addToLib('The Great Gatsby', 'F Scott Fitzgerald', '427', false);
+// addToLib('Example', 'Noone', '427', false);
+// addToLib('Hello', 'There', '427', false);
+// addToLib('Issme', 'Mario', '427', false);
